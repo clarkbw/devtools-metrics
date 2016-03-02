90e1f8494d504626facd5d001f0e75bf8dfeb552
Bryan Clark <clarkbw@gmail.com>
2016-03-02 09:05:33 -0800

    clean out unused panel graphs

--- js/app/empty-graphs.js
+++ js/app/empty-graphs.js
@@ -13,36 +13,6 @@ define('app/empty-graphs', ['jquery', 'MG'], function($, MG) {
       target: '#devtools-toolbox-opened-ratio-chart'
     });
 
-    var panels = ['inspector', 'console', 'debugger', 'network', 'performance', 'storage'];
-
-    panels.forEach((panel) => {
-      MG.data_graphic({
-        chart_type: 'missing-data',
-        width: 335,
-        height: 320,
-        left: 60,
-        animate_on_load: true,
-        target: '#devtools-toolbox-' + panel + '-panel-chart'
-      });
-    });
-
-    // MG.data_graphic({
-    //   chart_type: 'missing-data',
-    //   width: 515,
-    //   height: 320,
-    //   left: 60,
-    //   animate_on_load: true,
-    //   target: '#devtools-devedition-mau-chart'
-    // });
-    //
-    // MG.data_graphic({
-    //   chart_type: 'missing-data',
-    //   width: 515,
-    //   height: 320,
-    //   left: 60,
-    //   animate_on_load: true,
-    //   target: '#devtools-devedition-dau-chart'
-    // });
   }); // end $()
 
 }); // end define
