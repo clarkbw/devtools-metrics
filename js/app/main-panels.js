eeed836ae5206d0b79d4bb43ca30ca821e659022
Bryan Clark <clarkbw@gmail.com>
2016-03-04 09:58:06 -0800

    separate out panels and create links at bottom of index

--- js/app/main-panels.js
+++ js/app/main-panels.js
@@ -1,5 +1,5 @@
 /*global define, requirejs*/
-define('app/main-index', ['moment', 'TelemetryPromises'], function(moment, TelemetryPromises) {
+define('app/main-panels', ['moment', 'TelemetryPromises'], function(moment, TelemetryPromises) {
 
   // show 'a min' for least amount of time
   moment.relativeTimeThreshold('s', -1);
@@ -7,10 +7,8 @@ define('app/main-index', ['moment', 'TelemetryPromises'], function(moment, Telem
   moment.relativeTimeThreshold('m', 59);
 
   TelemetryPromises.init().then(() => {
-    requirejs(['app/empty-graphs']);
-    requirejs(['app/toolbox-opened']);
-    requirejs(['app/toolbox-opened-per-user']);
-    requirejs(['app/toolbox-time-active']);
+    requirejs(['app/panels-opened']);
+    requirejs(['app/panels-time-active']);
   });
 
 }); // end define
