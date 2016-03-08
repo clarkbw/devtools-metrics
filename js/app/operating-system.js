a0d9309cc665255a584eae2db00d2f5c6d31d76b
Bryan Clark <clarkbw@gmail.com>
2016-03-08 13:50:29 -0800

    add the OS env doughnut chart

--- js/app/operating-system.js
+++ js/app/operating-system.js
@@ -1,6 +1,6 @@
 /*global define*/
-define('app/operating-system', ['moment', 'lodash', 'd3', 'TelemetryPromises', 'DevToolsMetrics', 'LatestVersions'],
-function(moment, _, d3, T, DevToolsMetrics, LatestVersions) {
+define('app/operating-system', ['lodash', 'd3', 'TelemetryPromises', 'DevToolsMetrics', 'LatestVersions'],
+function(_, d3, T, DevToolsMetrics, LatestVersions) {
 
   var metric = 'DEVTOOLS_OS_ENUMERATED_PER_USER';
   var options = { sanitized: true };
@@ -10,7 +10,7 @@ function(moment, _, d3, T, DevToolsMetrics, LatestVersions) {
     title: 'Evolution of the DevTools Operating Systems (pre-release channels)',
     description: 'Telemetry numbers report the type of operating system according to our pre-defined buckets.',
     width: 335,
-    height: 320,
+    height: 450,
     left: 60,
     legend_target: '#' + ID + '-legend'
   };
@@ -45,6 +45,8 @@ function(moment, _, d3, T, DevToolsMetrics, LatestVersions) {
     }));
   }
 
+  // var ctx = document.getElementById("devtools-operating-system-donught").getContext("2d");
+
   DevToolsMetrics.line(ID, chart);
 
   LatestVersions.getLatestVersion().then((versions) => {
@@ -56,6 +58,24 @@ function(moment, _, d3, T, DevToolsMetrics, LatestVersions) {
     ).then((evolutions) => evolutionMap(evolutions)
     ).then((data) => {
       var grouped = _.groupBy(data, 'os');
+
+      // reduce the historgram values into one sum
+      var dough = _.map(grouped, function(value, index) {
+        return {
+          label: index,
+          value: value.reduce((p, c) => (p + c.value), 0)
+        };
+      });
+      // remove windows from the array to add back in later
+      var windows = _.remove(dough, function(value) {
+        return value.label.startsWith('Windows');
+      });
+      // add windows summing up the Win* values
+      dough.push({
+        label: 'Windows',
+        value: windows.reduce((p,c) => p + c.value, 0)
+      });
+      pie(dough);
       chart.legend = _.keys(grouped);
       return _.values(grouped);
     })
@@ -65,4 +85,126 @@ function(moment, _, d3, T, DevToolsMetrics, LatestVersions) {
     });
   });
 
+  function pie(data) {
+    var width = 530,
+    height = 400,
+    radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.category20();
+    var labels = data.map((d) => d.label);
+
+    var arc = d3.svg.arc()
+      .outerRadius(radius * 0.8)
+      .innerRadius(radius * 0.4);
+
+    var outerArc = d3.svg.arc()
+      .innerRadius(radius * 0.9)
+      .outerRadius((d) => {
+        // hack to make the OSX label not sit directly on the Linux one
+        return radius * (0.9 - labels.indexOf(d.data.label) / 10);
+      });
+
+    var pie = d3.layout.pie().sort(null).value((d) => d.value);
+
+    var svg = d3.select("#devtools-operating-system-doughnut")
+        .append("svg")
+        .append("g");
+
+    svg.append("g")
+       .attr("class", "slices");
+    svg.append("g")
+       .attr("class", "labels");
+    svg.append("g")
+       .attr("class", "lines");
+
+    svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    var key = (d) => d.data.label;
+
+    /* ------- PIE SLICES -------*/
+    var slice = svg.select(".slices").selectAll("path.slice")
+                    .data(pie(data), key);
+
+    slice.enter()
+      .insert("path")
+      .style("fill", (d) => color(d.data.label))
+      .attr("class", "slice");
+
+    slice
+      .transition().duration(1000)
+      .attrTween("d", function(d) {
+        this._current = this._current || d;
+        var interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return function(t) {
+          return arc(interpolate(t));
+        };
+      })
+
+    slice.exit()
+      .remove();
+
+    /* ------- TEXT LABELS -------*/
+
+    var text = svg.select(".labels").selectAll("text")
+      .data(pie(data), key);
+
+    text.enter()
+      .append("text")
+      .attr("dy", ".35em")
+      .text((d) => d.data.label);
+
+    function midAngle(d){
+      return d.startAngle + (d.endAngle - d.startAngle) / 2;
+    }
+
+    text.transition().duration(1000)
+      .attrTween("transform", function(d) {
+        this._current = this._current || d;
+        var interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return function(t) {
+          var d2 = interpolate(t);
+          var pos = outerArc.centroid(d2);
+          pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+          return "translate("+ pos +")";
+        };
+      })
+      .styleTween("text-anchor", function(d){
+        this._current = this._current || d;
+        var interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return function(t) {
+          var d2 = interpolate(t);
+          return midAngle(d2) < Math.PI ? "start":"end";
+        };
+      });
+
+    text.exit()
+      .remove();
+
+
+    /* ------- SLICE TO TEXT POLYLINES -------*/
+
+    var polyline = svg.select(".lines").selectAll("polyline")
+      .data(pie(data), key);
+
+    polyline.enter()
+      .append("polyline");
+
+    polyline.transition().duration(1000)
+      .attrTween("points", function(d){
+        this._current = this._current || d;
+        var interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return function(t) {
+          var d2 = interpolate(t);
+          var pos = outerArc.centroid(d2);
+          pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+          return [arc.centroid(d2), outerArc.centroid(d2), pos];
+        };
+      });
+
+    polyline.exit()
+      .remove();
+  }
 }); // end define
