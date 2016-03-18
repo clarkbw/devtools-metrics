ad669826c3e6dc45b07dea3c5d1e09fe6281984f
Bryan Clark <clarkbw@gmail.com>
2016-03-18 15:28:57 -0400

    move tilt to use library modules

--- js/app/main-tilt.js
+++ js/app/main-tilt.js
@@ -7,9 +7,30 @@ define('app/main-tilt', ['moment', 'TelemetryPromises'], function(moment, Teleme
   moment.relativeTimeThreshold('m', 59);
 
   TelemetryPromises.init().then(() => {
-    requirejs(['app/tilt-opened']);
-    requirejs(['app/tilt-opened-per-user']);
-    requirejs(['app/tilt-time-active']);
+    requirejs(['lib/telemetry/count', 'lib/telemetry/opened-per-user-flag'],
+    function (tc, opu) {
+      tc.graph(
+        'DEVTOOLS_TILT_OPENED_BOOLEAN',
+        'devtools-tilt-opened-chart', {
+          title: 'Tilt Opened Per User / Day (DAU)',
+          description: 'Telemetry numbers report how many times users have opened the DevTools tilt feature in the day.'
+        }, { sanitized: false });
+      opu.graph(
+        'DEVTOOLS_TILT_OPENED_PER_USER_FLAG',
+        'devtools-tilt-opened-per-user-chart', {
+          title: 'Tilt Opened per User / Release (MAU)',
+          description: 'Telemetry numbers report a user who has opened the DevTools tilt since the last release. (RAU)'
+        });
+        // TODO: engagement ratio?
+    });
+    requirejs(['lib/telemetry/time-active-seconds'],function (tac) {
+      tac.graph(
+        'DEVTOOLS_TILT_TIME_ACTIVE_SECONDS',
+        'devtools-tilt-time-active-chart', {
+          title: 'DevTools Tilt Active Time Spent',
+          least_squares: false
+        });
+    });
   });
 
 }); // end define
