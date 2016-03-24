546f9fff450b58c7e19b993f6ee21806d28d7aa3
Bryan Clark <clarkbw@gmail.com>
2016-03-24 07:53:16 -0700

    add note about the about:debugging post

--- js/app/main-about-debugging.js
+++ js/app/main-about-debugging.js
@@ -1,5 +1,6 @@
 /*global define, requirejs*/
-define('app/main-about-debugging', ['moment', 'TelemetryPromises'], function(moment, TelemetryPromises) {
+define('app/main-about-debugging', ['moment', 'TelemetryPromises', 'FIREFOX_RELEASES'],
+function(moment, TelemetryPromises, FIREFOX_RELEASES) {
 
   // show 'a min' for least amount of time
   moment.relativeTimeThreshold('s', -1);
@@ -7,17 +8,25 @@ define('app/main-about-debugging', ['moment', 'TelemetryPromises'], function(mom
   moment.relativeTimeThreshold('m', 59);
 
   TelemetryPromises.init().then(() => {
+    var markers = FIREFOX_RELEASES.aurora;
+    markers.push({
+      date: new Date('2016-03-10'),
+      label: 'Hacks Post [1]'
+    });
+
     requirejs(['lib/telemetry/count', 'lib/telemetry/opened-per-user-flag'],
     function (tc, opu) {
       tc.graph(
         'DEVTOOLS_ABOUTDEBUGGING_OPENED_COUNT',
         'devtools-about-debugging-opened-chart', {
-          title: 'about:debugging opened count'
+          title: 'about:debugging opened count',
+          markers: markers
         });
       opu.graph(
         'DEVTOOLS_ABOUTDEBUGGING_OPENED_PER_USER_FLAG',
         'devtools-about-debugging-opened-per-user-chart', {
-          title: 'about:debugging opened per user'
+          title: 'about:debugging opened per user',
+          markers: markers
         });
         // TODO: engagement ratio?
     });
