aa5009b70d251f0872340cb1f2a357d6c8e4d6c3
Bryan Clark <clarkbw@gmail.com>
2016-06-18 06:46:11 +0100

    move to DEVTOOLS_TOOLBOX_OPENED_COUNT

--- js/app/toolbox-opened.js
+++ js/app/toolbox-opened.js
@@ -2,18 +2,7 @@
 define('app/toolbox-opened', ['lodash', 'TelemetryPromises', 'DevToolsMetrics', 'LatestVersions', 'FIREFOX_RELEASES'],
 function(_, T, DevToolsMetrics, LatestVersions, FIREFOX_RELEASES) {
 
-  var metric = 'DEVTOOLS_TOOLBOX_OPENED_BOOLEAN';
-  var newMetric = 'DEVTOOLS_TOOLBOX_OPENED_COUNT';
-  // the old BOOLEAN metric is being phased out so this dictionary
-  // (assuming it is updated per release) will help us show activity as the
-  // new metric moves in and the old metric fades away.
-  var metrics = {
-    'nightly': newMetric,
-    'aurora': newMetric,
-    'beta': metric,
-    'release': metric
-  };
-
+  var metric = 'DEVTOOLS_TOOLBOX_OPENED_COUNT';
   var options = { sanitized: true };
   var markers = FIREFOX_RELEASES.beta;
   markers.push({
@@ -54,7 +43,7 @@ function(_, T, DevToolsMetrics, LatestVersions, FIREFOX_RELEASES) {
             DevToolsMetrics.progress(ID, total, current);
             return Promise.all(versions.map((target) => {
               return Promise.all(target.versions.map((version) => {
-                return T.getEvolution(target.channel, version, metrics[target.channel], options).then(function (evo) {
+                return T.getEvolution(target.channel, version, metric, options).then(function (evo) {
                   DevToolsMetrics.progress(ID, total, current -= 1);
                   return evo;
                 }).catch(function () {
